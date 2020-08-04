a0d6e1a4ae4dc9fc5ac648c0433cb81768cdf068
Dwaragesh Sivakumar <48896734+Dwaragesh@users.noreply.github.com>
2020-08-03 22:14:45 -0500

    Update Drone-Control.ino

--- Drone-Control.ino
+++ Drone-Control.ino
@@ -1,55 +1,59 @@
 /*
- * Drone control
- * 
- * This sketch receives Serial input values (from processing) and sends these values to the hacked controller.
- * Use this program only with the Arduino MKR1000 (or another 3.3 volt output based Arduino)
- * 
- * The circuit:
- * - 4 Low Pass filters with 100 µF capacitors and 220Ω resistors
- * - Hacked drone controller
- * 
+ * How does the mind-controlling drone operate?
+ * - This code is used for Arduino MKR 1000 through Arduino 1.8.9.
+     The purpose of the code is to receive the concentration and cortisol values of a user to send it to the controller circuit
  *
+ * - The drone will operate under the supervision of the user, who can see how they steer the drone via a terminal display of a live altimeter
+ * - Key notes:
+     A. Throtte and Yaw: Nose goes perpendicular to the y-axis, going left and right
+     B. Pitch and Roll: Nose goes perpendicular to the x-axis, going up and down
  *
  */
 
-// Set initial values
+// Set initial values for the drone to operate up, down, left, and right:
 int throttle  = 0;
-int yaw       = 127; // 3.3v / 2
+int yaw, pitch, roll;
+yaw = pitch = roll = 127;
 
-int pitch     = 127; // 3.3v / 2
-int roll      = 127; // 3.3v / 2
+// Values that are used for Pulse Width Modulation to create a low pass filter for each direction of the drone:
+int throttlePin   = 2;
+int yawPin        = 3;
+int pitchPin      = 4;
+int rollPin       = 5;
 
-int throttlePin   = 2; // PWM
-int yawPin        = 3; // PWM
-int pitchPin      = 4; // PWM
-int rollPin       = 5; // PWM
-
-
+// Constructing a foundation for the initialization of the drone's operation in the air:
 void setup() 
 {
   // Begin Serial communication at 115200 baud
+  // 115200 baud = 115200 bits per second
+  // Baud is the measure of the number of times a signal changes its state of movement per second
   Serial.begin( 115200 );
 
-  // Set pinModes
+  // Set pinModes for each direction
   pinMode( throttlePin,  OUTPUT );
   pinMode( yawPin,       OUTPUT );
   pinMode( pitchPin,     OUTPUT );
   pinMode( rollPin,      OUTPUT );
 }
 
+// Running for a certain period of time to keep the momentum of the mind-controlling drone:
+// Used to continously run the drone for the amount of baud it intakes:
 void loop() 
 {
-  // When there is an Serial connection available, get the values
+  // If there is a stable serial connection with the mind-controlling drone, then obtain the values:
   if ( Serial.available() > 0 ) 
   {
-    throttle    = Serial.parseInt();    // Store first interger value from Serial buffer
-    yaw         = Serial.parseInt();    // Store second interger value from Serial buffer
-    pitch       = Serial.parseInt();    // Store third interger value from Serial buffer
-    roll        = Serial.parseInt();    // Store fourth interger value from Serial buffer
+    // Serial buffer = a host can send indefinite data to a contraption
+    // A serial buffer led an array of generated inputs from the user to a hacked controller, resulting in a mind-controlling drone
+    throttle    = Serial.parseInt();    // Store the first, 8-bit integer value from the serial buffer
+    yaw         = Serial.parseInt();    // Store the second, 8-bit integer value from the serial buffer
+    pitch       = Serial.parseInt();    // Store the third, 8-bit integer value from the serial buffer
+    roll        = Serial.parseInt();    // Store the fourth, 8-bit integer value from the serial buffer
   }
  
   // Write values to the drone controller
-  // Use a low pass filter or DAC (digital to analog converter) to convert PWM to an analog voltage
+  // Use a low pass filter to convert Pulse Width Modulation (PWM) to an analog voltage
+  // Each value is designated for each direction:
   analogWrite( throttlePin,  throttle );
   analogWrite( yawPin,       yaw      );
   analogWrite( pitchPin,     pitch    );
